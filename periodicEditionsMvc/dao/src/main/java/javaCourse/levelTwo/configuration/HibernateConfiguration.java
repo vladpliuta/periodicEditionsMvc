0f50f9c1838f04fcc8a8e1684323ad5008db5c28
VladPliuta <vlad.pliuta@gmail.com>
2016-12-14 04:06:04 +0300

    структурные изменения

--- periodicEditionsMvc/dao/src/main/java/javaCourse/levelTwo/configuration/HibernateConfiguration.java
+++ periodicEditionsMvc/dao/src/main/java/javaCourse/levelTwo/configuration/HibernateConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+//import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -18,7 +18,7 @@ import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableAspectJAutoProxy
+//@EnableAspectJAutoProxy
 @EnableTransactionManagement
 @ComponentScan({"javaCourse.levelTwo.configuration"})
 @PropertySource(value = { "classpath:application.properties" })
